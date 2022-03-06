d81bfa1df010b6a5961fbd31647e119bdd3571cd
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-03-05 16:05:48 -0800

    dev environment setup

---
+++ src/client.js
@@ -0,0 +1,8 @@
+// import * as tf from '@tensorflow/tfjs';
+// import '@tensorflow/tfjs-backend-wasm';
+
+// console.log(tf.version);
+// tf.setBackend('wasm');
+// tf.ready().then(() => {
+//     console.log(tf.getBackend());
+// });
